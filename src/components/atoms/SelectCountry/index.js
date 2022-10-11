c5fff05d5dc3114cdfb4e3a8ded17a0a61bc1935
William Palomino <wpalomino@celerik.com>
2022-10-11 18:12:15 -0500

    feat: selectCountry and billinginformation

---
+++ src/components/atoms/SelectCountry/index.js
@@ -0,0 +1,77 @@
+import React, { useState, Fragment, useContext } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+
+// @ Data
+import { OrderContext } from '../../../context/OrderContext'
+
+const countries = [
+    'Mexico', 'Colombia', 'Canada'
+]
+
+
+const SelectCountry = () => {
+    const { billingData, setBillingData } = useContext(OrderContext);
+
+    const handleChange = (selected) => {
+        setBillingData(prevState => ({
+            ...prevState,
+            country: selected
+        }));
+    }
+
+    return (
+        <div>
+            <Listbox value={billingData.country} onChange={handleChange}>
+                <div className="relative mt-1">
+                    <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                        <span className="block truncate">{billingData.country || countries[0]}</span>
+                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                            <ChevronUpDownIcon
+                                className="h-5 w-5 text-gray-400"
+                                aria-hidden="true"
+                            />
+                        </span>
+                    </Listbox.Button>
+                    <Transition
+                        as={Fragment}
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-10">
+                            {countries.map((country, showIdx) => (
+                                <Listbox.Option
+                                    key={showIdx}
+                                    className={({ active }) =>
+                                        `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                                        }`
+                                    }
+                                    value={country}
+                                >
+                                    {({ selected }) => (
+                                        <>
+                                            <span
+                                                className={`block truncate ${selected ? 'font-medium' : 'font-normal'
+                                                    }`}
+                                            >
+                                                {country}
+                                            </span>
+                                            {selected ? (
+                                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                </span>
+                                            ) : null}
+                                        </>
+                                    )}
+                                </Listbox.Option>
+                            ))}
+                        </Listbox.Options>
+                    </Transition>
+                </div>
+            </Listbox>
+        </div>
+    )
+}
+
+export default SelectCountry
