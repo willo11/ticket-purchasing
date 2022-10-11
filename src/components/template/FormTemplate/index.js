0fb6077a0db54b61aa06f1692ff1b32c7883e3bc
William Palomino <wpalomino@celerik.com>
2022-10-11 13:45:12 -0500

    feat: set up text config, create Form Template and SelectShow component

---
+++ src/components/template/FormTemplate/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import language from '../../../data/text';
+import SelectShow from '../../atoms/SelectShow';
+
+const text = language.en
+
+const FormTemplate = () => {
+    return (
+        <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+            <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+                <div className="mx-auto max-w-md">
+                    <div className="text-center">
+                        <h4 className="text-xl font-semibold mt-1 mb-3 pb-1">{text.appName}</h4>
+                    </div>
+                    <div className="divide-y divide-gray-300/50">
+                        <div className="space-y-6 py-8 text-base leading-7 text-gray-600">
+                            <p>{text.form.messageSelect}</p>
+                            <div>
+                                <SelectShow />
+                            </div>
+                            <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.</p>
+                        </div>
+                        <div className="pt-8 text-base font-semibold leading-7">
+                            <p className="text-gray-900">Want to dig deeper into Tailwind?</p>
+                            <p>
+                                <a href="https://tailwindcss.com/docs" className="text-sky-500 hover:text-sky-600">Read the docs &rarr;</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FormTemplate
---
+++ src/data/shows/showsData.js
@@ -0,0 +1,70 @@
+export const showsData = [{
+    "id": 1,
+    "name": "Food, Inc.",
+    "price": "$8.73",
+    "date": "11/08/2021",
+    "notes": "xfr XFER Proof of at least one dose of COVID-19 vaccination for ages 5 to 11 and guest ages 12 and up will be required to show proof of two COVID-19 vaccine doses or one dose of the Johnson & Johnson vaccine."
+}, {
+    "id": 2,
+    "name": "Tapped",
+    "price": "$2.42",
+    "date": "08/28/2022",
+    "notes": "xfr XFER Proof of at least one dose of COVID-19 vaccination for ages 5 to 11 and guest ages 12 and up will be required to show proof of two COVID-19 vaccine doses or one dose of the Johnson & Johnson vaccine."
+
+}, {
+    "id": 3,
+    "name": "Secret in Their Eyes, The (El secreto de sus ojos)",
+    "price": "$7.64",
+    "date": "10/03/2022",
+    "notes": "xfr XFER Proof of at least one dose of COVID-19 vaccination for ages 5 to 11 and guest ages 12 and up will be required to show proof of two COVID-19 vaccine doses or one dose of the Johnson & Johnson vaccine."
+
+}, {
+    "id": 4,
+    "name": "Transfer",
+    "price": "$7.83",
+    "date": "12/05/2021",
+    "notes": "xfr XFER Proof of at least one dose of COVID-19 vaccination for ages 5 to 11 and guest ages 12 and up will be required to show proof of two COVID-19 vaccine doses or one dose of the Johnson & Johnson vaccine."
+
+}, {
+    "id": 5,
+    "name": "Castle on the Hudson",
+    "price": "$1.00",
+    "date": "03/26/2022",
+    "notes": "xfr XFER Proof of at least one dose of COVID-19 vaccination for ages 5 to 11 and guest ages 12 and up will be required to show proof of two COVID-19 vaccine doses or one dose of the Johnson & Johnson vaccine."
+
+}, {
+    "id": 6,
+    "name": "Ditchdigger's Daughters, The",
+    "price": "$9.44",
+    "date": "05/21/2022",
+    "notes": "xfr XFER Proof of at least one dose of COVID-19 vaccination for ages 5 to 11 and guest ages 12 and up will be required to show proof of two COVID-19 vaccine doses or one dose of the Johnson & Johnson vaccine."
+
+}, {
+    "id": 7,
+    "name": "Nenette and Boni (Nénette et Boni)",
+    "price": "$5.65",
+    "date": "03/09/2022",
+    "notes": "xfr XFER Proof of at least one dose of COVID-19 vaccination for ages 5 to 11 and guest ages 12 and up will be required to show proof of two COVID-19 vaccine doses or one dose of the Johnson & Johnson vaccine."
+
+}, {
+    "id": 8,
+    "name": "Attila (Attila the Hun)",
+    "price": "$1.68",
+    "date": "09/05/2022",
+    "notes": "xfr XFER Proof of at least one dose of COVID-19 vaccination for ages 5 to 11 and guest ages 12 and up will be required to show proof of two COVID-19 vaccine doses or one dose of the Johnson & Johnson vaccine."
+
+}, {
+    "id": 9,
+    "name": "Louise-Michel",
+    "price": "$1.10",
+    "date": "01/23/2022",
+    "notes": "xfr XFER Proof of at least one dose of COVID-19 vaccination for ages 5 to 11 and guest ages 12 and up will be required to show proof of two COVID-19 vaccine doses or one dose of the Johnson & Johnson vaccine."
+
+}, {
+    "id": 10,
+    "name": "Pressed",
+    "price": "$2.75",
+    "date": "05/22/2022",
+    "notes": "xfr XFER Proof of at least one dose of COVID-19 vaccination for ages 5 to 11 and guest ages 12 and up will be required to show proof of two COVID-19 vaccine doses or one dose of the Johnson & Johnson vaccine."
+
+}];
